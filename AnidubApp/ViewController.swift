f5c4ced274b36aa13112b9ab39549069ed3c0b2b
Roman <bolobolbfmv@gmail.com>
2017-04-18 18:43:07 +0300

    LeftSideMenu

--- AnidubApp/ViewController.swift
+++ AnidubApp/ViewController.swift
@@ -7,11 +7,94 @@
 //
 
 import UIKit
+import SideMenu
 
-class ViewController: UIViewController {
 
+struct NewsData{
+    var link:String
+    var title:String
+    var imagelink:String
+}
+
+extension String  {
+    var isNumber : Bool {
+        get{
+            return !self.isEmpty && self.rangeOfCharacter(from: CharacterSet.decimalDigits.inverted) == nil
+        }
+    }
+}
+extension UIImageView {
+    func downloadedFrom(url: URL, contentMode mode: UIViewContentMode = .scaleAspectFit) {
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() { () -> Void in
+                self.image = image
+            }
+            }.resume()
+    }
+    func downloadedFrom(link: String, contentMode mode: UIViewContentMode = .scaleAspectFit) {
+        guard let url = URL(string: link) else { return }
+        downloadedFrom(url: url, contentMode: mode)
+    }
+}
+
+
+extension UIImage {
+    
+    func imageResize (sizeChange:CGSize)-> UIImage{
+        
+        let hasAlpha = true
+        let scale: CGFloat = 0.0 // Use scale factor of main screen
+        
+        UIGraphicsBeginImageContextWithOptions(sizeChange, !hasAlpha, scale)
+        self.draw(in: CGRect(origin: CGPoint.zero, size: sizeChange))
+        
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        return scaledImage!
+    }
+    
+}
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var newsTableView: UITableView!
+
+    
+    var newsData: [NewsData] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadData()
+        // Define the menus
+        SideMenuManager.menuLeftNavigationController = storyboard!.instantiateViewController(withIdentifier: "LeftMenuNavigationController") as? UISideMenuNavigationController
+        
+        
+        
+        // Enable gestures. The left and/or right menus must be set up above for these to work.
+        // Note that these continue to work on the Navigation Controller independent of the View Controller it displays!
+
+        SideMenuManager.menuAddPanGestureToPresent(toView: self.navigationController!.navigationBar)
+        SideMenuManager.menuAddScreenEdgePanGesturesToPresent(toView: self.navigationController!.view)
+        let appScreenRect: CGRect = UIScreen.main.bounds
+        SideMenuManager.menuWidth = max(round(min((appScreenRect.width), (appScreenRect.height)) * 0.15), 240)
+        //SideMenuManager.menuShadowColor = UIColor(red: 41 , green: 182, blue: 246, alpha: 1)
+        SideMenuManager.menuAnimationBackgroundColor = UIColor(red: 66.0/255 , green: 66.0/255 , blue: 66.0/255 , alpha: 1.0)
+        SideMenuManager.menuAllowPushOfSameClassTwice = false
+        SideMenuManager.menuFadeStatusBar = false
+        
+        // Data model: These strings will be the data for the table view cells
+        
+        // Register the table view cell class and its reuse id
+
+        // This view controller itself will provide the delegate methods and row data for the table view.
+        newsTableView.delegate = self
+        newsTableView.dataSource = self
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +103,48 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func loadData()
+    {
+        let url = "http://vstu.ru/archive/news/index.html"
+        
+        
+        guard let myURL = URL (string : url) else {
+            print ("Error")
+            return
+        }
+        
+        
+        
+    }
+    
+    // number of rows in table view
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (self.newsData.count)
+    }
+    
+    // create a cell for each table view row
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // create a new cell if needed or reuse an old one
+        let cell = self.newsTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        // set the text from the data model
+        cell.textLabel?.font = cell.textLabel?.font.withSize(14)
+        cell.textLabel?.text = self.newsData[indexPath.row].title
+        let  image : UIImage = UIImage(named: "Elefant")!
+        cell.imageView?.downloadedFrom(link: self.newsData[indexPath.row].imagelink)
+        cell.imageView?.image = image.imageResize(sizeChange: CGSize(width: 80, height: 80))
+        return (cell)
+    }
+    
+    // method to run when table view cell is tapped
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You tapped cell number \(indexPath.row).")
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 83
+    }
 
 }
 
---
+++ AnidubApp/globalfunctions.swift
@@ -0,0 +1,167 @@
+//
+//  globalfunctions.swift
+//  Vstu
+//
+//  Created by Roman Efimov on 18.02.17.
+//  Copyright © 2017 Roman Efimov. All rights reserved.
+//
+
+import Foundation
+import SQLite
+
+
+func saveStudent(){
+    var path = getPath(fileName: "appdatabase.sqlite")
+    
+    let db = try! Connection(getPath(fileName: "appdatabase.sqlite"))
+    let student = Table("Student")
+    
+    let id = Expression<Int>("id")
+    let Recordbook = Expression<Int>("Recordbook")
+    let Group = Expression<String?>("Group")
+    let Name = Expression<String>("Name")
+    let Surname = Expression<String>("Surname")
+    let Middlename = Expression<String>("Middlename")
+    
+    
+   // let insert = student.insert(id <- value.id, Recordbook <- value.RecordBook, Group <- value.Group, Name <- value.Name, Surname <- value.Surname, Middlename <- value.MiddleName)
+    //let rowid = try! db.run(insert)
+}
+
+func removeStudent(){
+    let db = try! Connection(getPath(fileName: "appdatabase.sqlite"))
+    let student = Table("Student")
+    
+    let alice = student.filter(1 == rowid)
+    
+    try! db.run(alice.delete())
+
+    
+}
+
+func loadStudent() -> Any? {
+    let db = try! Connection(getPath(fileName: "appdatabase.sqlite"))
+    let student = Table("Student")
+    
+    let id = Expression<Int>("id")
+    let Recordbook = Expression<Int>("Recordbook")
+    let Group = Expression<String?>("Group")
+    let Name = Expression<String>("Name")
+    let Surname = Expression<String>("Surname")
+    let Middlename = Expression<String>("Middlename")
+    
+    for student in try! db.prepare(student) {
+        let id = student.get(id)
+        let name =  student.get(Name)
+        let surname =  student.get(Surname)
+        let middlename =  student.get(Middlename)
+        let group = student.get(Group)
+        let recordbook = student.get(Recordbook)
+        // id: 1, email: alice@mac.com, name: Optional("Alice")
+        return nil // Student(id: id, Name: name, Surname: surname, MiddleName: middlename, Group: group!, RecordBook: recordbook)
+    }
+    
+    return nil
+    
+}
+func getPath(fileName: String) -> String {
+    let documentsURL = NSURL(
+        fileURLWithPath: NSSearchPathForDirectoriesInDomains(
+            .documentDirectory, .userDomainMask, true).first!,
+        isDirectory: true
+    )
+    
+    let fileURL = documentsURL.appendingPathComponent(fileName)
+    
+    var fileSize : UInt64 = 1
+    
+    do {
+        //return [FileAttributeKey : Any]
+        let attr = try FileManager.default.attributesOfItem(atPath: (fileURL?.path)!)
+        fileSize = attr[FileAttributeKey.size] as! UInt64
+        
+        //if you convert to NSDictionary, you can get file size old way as well.
+        let dict = attr as NSDictionary
+        fileSize = dict.fileSize()
+    } catch {
+        print("Error: \(error)")
+    }
+    if(fileSize == 1)
+    {
+        print("fail")
+        return ""
+    }
+    
+    return fileURL!.path
+}
+
+func copyFile(fileName: NSString) {
+    
+    let fileManager = FileManager.default
+    let documentsPath = NSURL(fileURLWithPath: NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0])
+    let destinationSqliteURL = documentsPath.appendingPathComponent("appdatabase.sqlite")
+    let sourceSqliteURL = Bundle.main.url(forResource: "appdatabase", withExtension: "sqlite")
+    
+    if !fileManager.fileExists(atPath: destinationSqliteURL!.path) {
+        // var error:NSError? = nil
+        do {
+            try fileManager.copyItem(at: sourceSqliteURL!, to: destinationSqliteURL!)
+            print("Copied")
+            print(destinationSqliteURL?.path)
+        } catch let error as NSError {
+            print("Unable to create database \(error.debugDescription)")
+        }
+    }
+}
+
+func getDataFromDatabase(recordbook : Int)
+{
+    
+    var request = URLRequest(url: URL(string: "http://185.58.205.8/GetInfo.php")!)
+    request.httpMethod = "POST"
+    var bodyData = "recordbook=\(recordbook)"
+    
+    
+    request.httpBody = bodyData.data(using: String.Encoding.utf8)
+    let semaphore = DispatchSemaphore(value: 0)
+    
+    let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        guard let data = data else {
+            print("request failed \(error)")
+            return
+        }
+        
+        do {
+            
+            if let convertedJsonIntoDict = try JSONSerialization.jsonObject(with: data, options: []) as? NSArray {
+                
+                // Print out dictionary
+                print(convertedJsonIntoDict)
+                
+                // Get value by key
+                let id = (convertedJsonIntoDict[0] as! NSDictionary)["id"] as? String
+                let name = (convertedJsonIntoDict[0] as! NSDictionary)["Name"] as? String
+                let surname = (convertedJsonIntoDict[0] as! NSDictionary)["Surname"] as? String
+                let middlename = (convertedJsonIntoDict[0] as! NSDictionary)["MiddleName"] as? String
+                let group = (convertedJsonIntoDict[0] as! NSDictionary)["GroupName"] as? String
+                let recordbook = (convertedJsonIntoDict[0] as! NSDictionary)["RecordBook"] as? String
+                
+                
+                //var student = Student(id:Int(id!)!,Name: name!,Surname: surname!,MiddleName: middlename!,Group: group!,RecordBook: Int(recordbook!)!)
+                
+                saveStudent()
+            }
+            semaphore.signal()
+            
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        
+    }
+    task.resume()
+    
+    _ = semaphore.wait(timeout: .distantFuture)
+    
+    return
+    
+}
